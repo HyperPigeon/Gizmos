e06ad5418e0f0589ce582d01b2d6bcee88a12daf
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2022-03-09 11:39:27 -0500

    fixed server crash bug
    fixed server crash bug caused by Line 26 in Gizmos.java using net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry

--- src/main/java/net/hyper_pigeon/Gizmos/Gizmos.java
+++ src/main/java/net/hyper_pigeon/Gizmos/Gizmos.java
@@ -2,14 +2,12 @@ package net.hyper_pigeon.Gizmos;
 
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
 import net.hyper_pigeon.Gizmos.config.GizmosConfig;
 import net.hyper_pigeon.Gizmos.mixin.MixinConditions;
 import net.hyper_pigeon.Gizmos.registry.GizmoBlocks;
 import net.hyper_pigeon.Gizmos.registry.GizmoEnchantments;
 import net.hyper_pigeon.Gizmos.registry.GizmoEntities;
 import net.hyper_pigeon.Gizmos.registry.GizmoItems;
-import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -23,42 +21,6 @@ public class Gizmos implements ModInitializer {
 		GizmoItems.init();
 		GizmoEntities.init();
 		GizmoBlocks.init();
-		FabricModelPredicateProviderRegistry.register(GizmoItems.SOUL_FIRE_SPITTER, new Identifier("tank_amount"),
-				(itemStack, clientWorld, livingEntity, i) -> {
-					if(itemStack.getDamage() >= itemStack.getMaxDamage()) {
-						return 0.0F;
-					}
-					else if (itemStack.getDamage() >= 3*(itemStack.getMaxDamage()/4)){
-						return 1.0F;
-					}
-					else if (itemStack.getDamage() >= itemStack.getMaxDamage()/2){
-						return 2.0F;
-					}
-					else if (itemStack.getDamage() >= itemStack.getMaxDamage()/4){
-						return 3.0F;
-					}
-					else {
-						return 4.0F;
-					}
-				});
-
-		FabricModelPredicateProviderRegistry.register(GizmoItems.SLINGSHOT, new Identifier("slingshot_pull"),
-				(itemStack, clientWorld, livingEntity, i) -> {
-					if (livingEntity == null) {
-						return 0.0F;
-					}
-					else {
-						return livingEntity.getActiveItem() != itemStack ? 0.0F : (float)(itemStack.getMaxUseTime() - livingEntity.getItemUseTimeLeft()) / 20.0F;
-					}
-				});
-
-		FabricModelPredicateProviderRegistry.register(GizmoItems.SLINGSHOT, new Identifier("slingshot_pulling"),
-				(itemStack, clientWorld, livingEntity, i) -> {
-					return livingEntity != null && livingEntity.isUsingItem() && livingEntity.getActiveItem() == itemStack ? 1.0F : 0.0F;
-				});
-
 		GizmoEnchantments.init();
-
-
 	}
 }
